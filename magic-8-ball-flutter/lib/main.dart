bbce61d880556f9391b2f00577d87a870fcf7993
KangDain <Dmelli0505@gmail.com>
2020-01-14 22:46:45 +0900

    test git

---
+++ magic-8-ball-flutter/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+   home: AnswerPage(),
+  ),
+);
+
+class AnswerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange[100],
+      appBar: AppBar(
+        backgroundColor: Colors.orange[200],
+        title: Text('Ask me ANYTHING'),
+      ),
+      body: Answer(),
+    );
+  }
+}
+
+
+class Answer extends StatefulWidget {
+  @override
+  _AnswerState createState() => _AnswerState();
+}
+
+class _AnswerState extends State<Answer> {
+  
+    int answerNum = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: (){
+          setState(() {
+            answerNum = Random().nextInt(5)+1;
+          });
+        },
+        child: Image.asset('images/ball$answerNum.png'),
+      ),
+    );
+  }
+}
+
