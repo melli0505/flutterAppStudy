669fd7577e9c509c15a1846941a30a35c86b4fb8
Kang Dain <dmelli0505@gmail.com>
2022-01-14 01:03:33 +0900

    update flutter

--- choose_your_destiny/ios/Flutter/flutter_export_environment.sh
+++ choose_your_destiny/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\KangDain\flutter_windows_v1.9.1+hotfix.2-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\KangDain\.vscode\flutterStudy\choose_your_destiny"
+export "FLUTTER_ROOT=C:\Users\KangDain\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\KangDain\Desktop\flutter-study\choose_your_destiny"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\KangDain\flutter_windows_v1.9.1+hotfix.2-stable\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
