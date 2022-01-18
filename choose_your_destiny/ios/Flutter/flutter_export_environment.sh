6bef18a0a7bce7fd655c124b22b9cdf18772be95
Kang Dain <dmelli0505@gmail.com>
2022-01-18 23:46:15 +0900

    update flutter environment

--- choose_your_destiny/ios/Flutter/flutter_export_environment.sh
+++ choose_your_destiny/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\KangDain\flutter"
+export "FLUTTER_ROOT=C:\Users\KangDain\flutter\flutter"
 export "FLUTTER_APPLICATION_PATH=C:\Users\KangDain\Desktop\flutter-study\choose_your_destiny"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
